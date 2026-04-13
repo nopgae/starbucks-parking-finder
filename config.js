3cece59708af82b0af465e9ecc21737c39e88e42
nopgae <nopgae@users.noreply.github.com>
2026-04-13 13:08:24 +0000

    deploy: 8871b5cf23e64be911d65823b2a908ee1cbb17b5

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const GOOGLE_MAPS_API_KEY = "";
-const API_BASE = "";
+const GOOGLE_MAPS_API_KEY = "AIzaSyCBlZv0v6_ctiJeHr9c3ptBOktSq9byx5s";
+const API_BASE = "https://starbucks-parking-finder.onrender.com";
